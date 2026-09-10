af6ec6472698a0dc23066dcb07d9f2d481ad9841
Adedolapo Okunsanmi <AdedolapoO@rystadenergy.com>
2026-09-10 13:04:07 +0200

    personalize manifests

--- personalize.sh
+++ personalize.sh
@@ -5,8 +5,9 @@ read username
 
 ghcr_username=$(echo "$username" | tr '[:upper:]' '[:lower:]')
 
-find . -type f -name '*.yaml' -exec sed -E -i '' s#https://github.com/[-_a-zA-Z0-9]+#https://github.com/${username}#g {} +
-find . -type f -name '*.yaml' -exec sed -E -i '' s#ghcr.io/[-_a-zA-Z0-9]+#ghcr.io/${ghcr_username}#g {} +
+find . -type f -name '*.yaml' -exec sed -E -i.bak s#https://github.com/[-_a-zA-Z0-9]+#https://github.com/${username}#g {} +
+find . -type f -name '*.yaml' -exec sed -E -i.bak s#ghcr.io/[-_a-zA-Z0-9]+#ghcr.io/${ghcr_username}#g {} +
+find . -type f -name '*.yaml.bak' -delete
 
 echo "Enter Argo CD destination name or server where applications will be deployed (e.g. in-cluster, https://kubernetes.default.svc)"
 echo -n "Destination: "
